21b1fd03441c94083d9ba99da49d2035d36cf0c4
Neo Xu <neo.xu@gmail.com>
2024-12-06 00:01:28 +0800

    Send signal to process before and after operate the button.

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 There's `reference-on.png` and `reference-off.png` that are captured using this script.
 It's important to use this tool instead of other screenshot tool to make sure pixel is exactly the same.
 
+`psutil` is used to send signal to process before operating the button.
+
 ## Requirements
 
 ```bash
 sudo apt install gnome-screenshot python3-tk python3-dev python3-pip
-pip install pyautogui
+pip install pyautogui psutil
 ```
--- clicker.py
+++ clicker.py
@@ -1,5 +1,8 @@
 import time
 import pyautogui
+import psutil
+import os
+import signal
 
 # Generate the reference image
 # screenshot = pyautogui.screenshot(region=(414-25,148-13, 50, 26))
@@ -23,7 +26,28 @@ def get_is_button_off():
         return
     return location
 
+def send_signal(procname, sig=signal.SIGSTOP):
+    # Iterate over all running processes
+    for proc in psutil.process_iter(['pid', 'name']):
+        try:
+            # Check if process name contains the given substring
+            if procname in proc.info['name']:
+                print(f"Sending signal {sig} to process {proc.info['name']} (PID: {proc.info['pid']})")
+                try:
+                    os.kill(proc.info['pid'], sig)
+                except PermissionError:
+                    pass
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    print(f"Sent to '{procname}'")
+    return False
+
+
 while True:
+    # Wakeup the process
+    send_signal("corplink", sig=signal.SIGCONT)
+    time.sleep(5)
+
     # Turn on the button
     if location := get_is_button_on():
         # Need to turn off firstly
@@ -39,4 +63,6 @@ while True:
     # Turn on
     print("Turn on the button", flush=True)
     pyautogui.click(location)
+    time.sleep(5)
+    send_signal("corplink", sig=signal.SIGSTOP)
     time.sleep(60 * 60 * 10)  # Toggle every 10 hours
