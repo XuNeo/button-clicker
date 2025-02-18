14fe7174c3d6f704969ac7ed93a428489c0ac78f
Neo Xu <neo.xu1990@gmail.com>
2025-02-18 01:07:50 +0000

    Use logging instead of printf and add timestamp
    Signed-off-by: Neo Xu <neo.xu1990@gmail.com>

--- clicker.py
+++ clicker.py
@@ -3,6 +3,7 @@ import pyautogui
 import psutil
 import os
 import signal
+import logging
 
 # Generate the reference image
 # screenshot = pyautogui.screenshot(region=(414-25,148-13, 50, 26))
@@ -10,7 +11,10 @@ import signal
 
 # Locate the reference image on the screen
 
-print("Start the script", flush=True)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info("Hello")
+logging.info("Start the script")
 
 def get_is_button_on():
     try:
@@ -32,37 +36,37 @@ def send_signal(procname, sig=signal.SIGSTOP):
         try:
             # Check if process name contains the given substring
             if procname in proc.info['name']:
-                print(f"Sending signal {sig} to process {proc.info['name']} (PID: {proc.info['pid']})")
+                logging.info(f"Sending signal {sig} to process {proc.info['name']} (PID: {proc.info['pid']})")
                 try:
                     os.kill(proc.info['pid'], sig)
                 except PermissionError:
                     pass
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    print(f"Sent to '{procname}'")
+    logging.info(f"Sent to '{procname}'")
     return False
 
 
 while True:
     # Wakeup the process
     send_signal("corplink", sig=signal.SIGCONT)
-    time.sleep(5)
+    time.sleep(8)
 
     # Turn on the button
     if location := get_is_button_on():
         # Need to turn off firstly
-        print("Turn off the button", flush=True)
+        logging.info("Turn off the button")
         pyautogui.click(location)
-        time.sleep(5)
+        time.sleep(8)
     else:
         location = get_is_button_off()
         if not location:
-            print("Reference image not found on the screen.")
+            logging.info("Reference image not found on the screen.")
             break
 
     # Turn on
-    print("Turn on the button", flush=True)
+    logging.info("Turn on the button")
     pyautogui.click(location)
-    time.sleep(5)
+    time.sleep(8)
     send_signal("corplink", sig=signal.SIGSTOP)
-    time.sleep(60 * 60 * 10)  # Toggle every 10 hours
+    time.sleep(60 * 60 * 18)  # Toggle every 10 hours
